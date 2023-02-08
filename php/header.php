13e5b024395e5368ccaad8ac97b3229065da4968
jpm861003 <jorge.pm@ceiich.unam.mx>
2023-02-08 15:51:15 -0600

    Header responsivo

--- php/header.php
+++ php/header.php
@@ -1,10 +1,10 @@
 <header>
     <div class="logoUnam">
         <a href="https://www.unam.mx/"><img src="./src/images/Logo-UNAM.png" alt="LogoUnam.png"></a>
-        <a href="../index.php"><img src="./src/images/Logo-CEIICH.png" alt="CEIICH" width="100" height="115"></a>
-        <a href="../index.php" class="letter2">CENTRO DE INVESTIGACIONES INTERDISCIPLINARIAS<br> EN CIENCIAS Y HUMANIDADES</a>
+        <a href="https://www.ceiich.unam.mx/"><img src="./src/images/Logo-CEIICH.png" alt="CEIICH"></a>
+        <a href="https://www.ceiich.unam.mx/" class="letter2">CENTRO DE INVESTIGACIONES INTERDISCIPLINARIAS<br> EN CIENCIAS Y HUMANIDADES</a>
     </div>
-    <div class="logoCeiich">
-        <a href="../index.php"><img src="./src/images/grid-nav.png" alt="CEIICH" width="1000" height="200"></a>
+    <div class="mosaicoCeiich">
+        <a href="../index.php"><img src="./src/images/grid-nav.png" alt="CEIICH" ></a>
     </div>
 </header>
